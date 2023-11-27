c17a5d9412ed2d64366e98b1a732cc5cd490e58d
Niranjan <104029594+Niranjanravichandran18@users.noreply.github.com>
2023-11-27 12:32:26 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from "./components/App.jsx";
 import {BrowserRouter} from 'react-router-dom'
 import "./components/css/Style.css"
 
-
 const root=ReactDOM.createRoot(document.getElementById("root"))
 root.render(
 <React.StrictMode>
